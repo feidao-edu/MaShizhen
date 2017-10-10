0c04fd2ac03429a072578816269f393d9fb753da
MaShizhen <2473872761@qq.com>
2017-10-10 11:45:53 +0800

    123124

--- 20171010/dist/app.js
+++ 20171010/dist/app.js
@@ -1,15 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var express = require("express");
-var bodyParser = require("body-parser");
 // var bodyParser = require('body-parser');
 var app = express();
-// json类型body
-app.use(bodyParser.json());
-// query string类型body
-app.use(bodyParser.urlencoded({
-    extended: false
-}));
 // 静态文件目录
 app.use(express.static(__dirname + '../public'));
 // 路由与业务逻辑
--- 20171010/src/app.ts
+++ 20171010/src/app.ts
@@ -1,13 +1,6 @@
 import * as  express from 'express';
-import * as bodyParser from 'body-parser'
 // var bodyParser = require('body-parser');
 var app = express();
-// json类型body
-app.use(bodyParser.json());
-// query string类型body
-app.use(bodyParser.urlencoded({
-    extended: false
-}));
 // 静态文件目录
 app.use(express.static(__dirname + '../public'));
 // 路由与业务逻辑
