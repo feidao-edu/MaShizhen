692c4b8fc86c561fbd49d5bf2a447a4ea29589d5
taoqf <tao_qiufeng@126.com>
2017-10-10 18:05:29 +0800

    ignore files

--- dist/module/routes/users.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-var express_1 = require("express");
-var router = express_1.Router();
-router.get('/add', function (req, res) {
-    res.send(req.query);
-});
-module.exports = router;
--- dist/module/use.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-// import add2 from './add';
-// 对应的函数如下
-// export default function add2(x: number, y: number) {
-//     return x + y;
-// }
